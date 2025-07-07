fff0529a7326fc6ee9a40733ddce4173c4975935
zainirshad1 <zain.pate@gmail.com>
2025-07-07 12:48:13 +0530

    Create common.js

---
+++ common.js
@@ -0,0 +1,10 @@
+// common.js - Shared functions across all views
+function updateCurrentTime() {
+    // Time updating logic
+}
+
+function updateMosquePrayerTimes(mosqueId) {
+    // Prayer times logic
+}
+
+// Other shared functions...
