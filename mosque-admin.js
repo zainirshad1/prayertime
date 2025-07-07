bd927c80821c64c2d42abc6f4839c53a19ac5111
zainirshad1 <zain.pate@gmail.com>
2025-07-07 12:50:01 +0530

    Create mosque-admin.js

---
+++ mosque-admin.js
@@ -0,0 +1,6 @@
+// mosque-admin.js - Mosque admin functionality
+function showMosqueAdminView() {
+    document.getElementById('content-container').innerHTML = `
+        <!-- Mosque admin content -->
+    `;
+}
